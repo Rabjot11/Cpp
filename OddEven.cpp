a2749bf79fe5185e85093fc2dd36f68e797923f4
Rabjot11 <singhrabjot738@gmail.com>
2023-06-29 23:14:33 +0530

    Even Odd

---
+++ OddEven.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    
+    int n;
+    cout<<"Enter a Number:-";
+    cin>>n;
+
+    if(n%2==0){
+
+        cout<<"The given number is Even"<<endl;
+
+    }
+    else{
+
+        cout<<"The given number is Odd"<<endl;
+
+    }
+    
+
+}
