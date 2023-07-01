4af95a375bab3a57ae2090949f78e76b6bedc9af
Rabjot11 <singhrabjot738@gmail.com>
2023-07-01 23:39:42 +0530

    Pre Increment operator

---
+++ operator(Pre Increment).cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+// Binaary operator +, -, *, /, %
+// Unary operator ++, --
+
+int main(){
+    int a;
+    cin>> a;
+    int b;
+    b= a++;  //Pre Increment 
+    cout<<a<<" "<<b<<endl;
+    return 0;
+
+}
