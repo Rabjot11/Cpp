a169de82867717fd802769a3d7a037cfea8d61b3
Rabjot11 <singhrabjot738@gmail.com>
2023-07-01 23:41:23 +0530

    Post Increment Operator

---
+++ Operator(Post Increment).cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+// Binaary operator +, -, *, /, %
+// Unary operator ++, --
+
+
+
+int main(){
+    int c;
+    cin>> c;
+    int d;
+    d= ++c;  //Post Increment 
+    cout<<c<<" "<<d<<endl;
+    return 0;
+}
